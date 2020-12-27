b51b9166aa93a7aa31411239d2697ab5261832fe
Omer Amar <omer.amar@intel.com>
2020-12-27 20:21:59 +0200

    create runners

---
+++ runners/dataframe_runner.py
@@ -0,0 +1,40 @@
+from hierarchy_manager.dataframe_hierarchy_manager import DataFrameHierarchyManager
+
+
+path = '/Users/oamar/Documents/Interviews/YotpoHomeAsssignment/data/hierarchy.txt'
+hm = DataFrameHierarchyManager(hierarchy_path=path)
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('yotpo')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('my tasty bread')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('board games')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('coffee and tea')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('cookies and candy')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('bread and water')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels("bakery and cookies omer amar cakes")
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
---
+++ runners/tree_runner_format_1.py
@@ -0,0 +1,40 @@
+from hierarchy_manager.tree_hierarchy_manager import TreeHierarchyManagerFormatOne
+
+
+path = '/Users/oamar/Documents/Interviews/YotpoHomeAsssignment/data/hierarchy.txt'
+hm = TreeHierarchyManagerFormatOne(hierarchy_path=path)
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('yotpo')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('my tasty bread')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('board games')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('coffee and tea')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('cookies and candy')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels('bread and water')
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
+
+extracted_labels, specific_labels, chosen_label = hm.detect_labels("bakery and cookies omer amar cakes")
+print(f'extracted labels: {extracted_labels}, '
+      f'specific labels: {specific_labels}, '
+      f'chosen label: {chosen_label}')
